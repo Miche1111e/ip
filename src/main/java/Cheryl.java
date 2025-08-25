c98b5c45770034742efce7d0a0eabc487dddf077
Michelle <16mchllliew11@gmail.com>
2025-08-25 21:43:11 +0800

    Add Todo, Event, Deadline

--- src/main/java/Cheryl.java
+++ src/main/java/Cheryl.java
@@ -17,11 +17,56 @@ class Task {
         isDone = false;
     }
 
+    public String getTypeIcon() {
+        return "[ ]";
+    }
     @Override
     public String toString() {
         return "[" + (isDone ? "X" : " ") + "] " + title;
     }
 }
+
+class Todo extends Task {
+    Todo(String title) {
+        super(title);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
+
+class Deadline extends Task {
+    String dueDate;
+
+    Deadline(String title, String dueDate) {
+        super(title);
+        this.dueDate = dueDate;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + dueDate + ")";
+    }
+}
+
+class Event extends Task {
+    String from;
+    String to;
+
+    Event(String title, String from, String to) {
+        super(title);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+    }
+}
+
 
 public class Cheryl {
     public static void main(String[] args) {
@@ -59,8 +104,40 @@ public class Cheryl {
                 } catch (Exception e) {
                     System.out.println("Invalid task number");
                 }
+            } else if (cmd.startsWith("todo ")) {
+                String title = cmd.substring(5).trim();
+                tasks.add(new Todo(title));
+                System.out.println("Got it. I've added this task:");
+                System.out.println(tasks.get(tasks.size()-1));
+                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+            } else if (cmd.startsWith("deadline ")) {
+                try {
+                    String[] parts = cmd.substring(9).split("/by", 2);
+                    String title = parts[0].trim();
+                    String by = parts[1].trim();
+                    tasks.add(new Deadline(title, by));
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(tasks.get(tasks.size()-1));
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                } catch (Exception e) {
+                    System.out.println("Invalid deadline format! Use: deadline <title> /by <date>");
+                }
+            } else if (cmd.startsWith("event ")) {
+                try {
+                    String[] parts = cmd.substring(6).split("/from", 2);
+                    String title = parts[0].trim();
+                    String[] times = parts[1].split("/to", 2);
+                    String from = times[0].trim();
+                    String to = times[1].trim();
+                    tasks.add(new Event(title, from, to));
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(tasks.get(tasks.size()-1));
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                } catch (Exception e) {
+                    System.out.println("     Invalid event format! Use: event <title> /from <start> /to <end>");
+                }
             } else {
-                tasks.add(new Task(cmd));
+                System.out.println("Invalid command");
             }
         }
         sc.close();
