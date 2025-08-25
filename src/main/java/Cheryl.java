70d5b83799db7e1ba7f934448bfb04e8f0bc4c3e
Michelle <16mchllliew11@gmail.com>
2025-08-25 16:45:33 +0800

    Implement echo version

--- src/main/java/Cheryl.java
+++ src/main/java/Cheryl.java
@@ -1,7 +1,21 @@
-public class Duke {
+import java.util.Scanner;
+
+public class Cheryl {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Hello! I'm Cheryl");
         System.out.println("What can I do for you?");
-        System.out.println("");
+
+        while(true) {
+            String cmd = sc.nextLine();
+            if (cmd.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(cmd);
+            }
+        }
+        sc.close();
     }
 }
