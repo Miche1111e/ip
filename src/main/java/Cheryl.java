7f7771bf5bf9bf885012b5d660c0d4a88ae24e24
Michelle <16mchllliew11@gmail.com>
2025-08-25 17:20:23 +0800

    Add list

--- src/main/java/Cheryl.java
+++ src/main/java/Cheryl.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Cheryl {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> inputs = new ArrayList<>();
 
         System.out.println("Hello! I'm Cheryl");
         System.out.println("What can I do for you?");
@@ -12,8 +14,12 @@ public class Cheryl {
             if (cmd.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (cmd.equals("list")){
+                for(int i = 0; i < inputs.size(); i++) {
+                    System.out.println((i + 1) + "." + inputs.get(i));
+                }
             } else {
-                System.out.println(cmd);
+                inputs.add(cmd);
             }
         }
         sc.close();
