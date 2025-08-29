5a53a32ec9638965fae97ffa32a7ed87adc13b54
Michelle <16mchllliew11@gmail.com>
2025-08-29 18:02:07 +0800

    Add JavaDoc for Task, Todo, Event, Deadline

--- src/main/java/cheryl/task/Event.java
+++ src/main/java/cheryl/task/Event.java
@@ -1,23 +1,48 @@
 package cheryl.task;
 
+/**
+ * Represents an event task with a start and end time.
+ */
 public class Event extends Task {
     String from;
     String to;
 
+    /**
+     * Creates a new Event task.
+     *
+     * @param title The title of the event
+     * @param from  The start time of the event
+     * @param to    The end time of the event
+     */
     public Event(String title, String from, String to) {
         super(title);
         this.from = from;
         this.to = to;
     }
 
+    /**
+     * Returns the start time of the event.
+     *
+     * @return the start time string
+     */
     public String getFrom() {
         return from;
     }
 
+    /**
+     * Returns the end time of the event.
+     *
+     * @return the end time string
+     */
     public String getTo() {
         return to;
     }
 
+    /**
+     * Returns a string representation of the event.
+     *
+     * @return String in the format "[E][ ] title (from: from to: to)"
+     */
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
--- src/main/java/cheryl/task/Task.java
+++ src/main/java/cheryl/task/Task.java
@@ -1,24 +1,60 @@
 package cheryl.task;
 
+/**
+ * Represents a generic task with a description and completion status.
+ */
 public class Task {
     String title;
     boolean isDone;
 
-    Task (String title) {
+    /**
+     * Creates a new Task with the given title.
+     *
+     * @param title The description/title of the task
+     */
+
+     public Task (String title) {
         this.title = title;
         this.isDone = false;
     }
 
+    /**
+     * Marks the task as done.
+     */
     public void mark() {
         this.isDone = true;
     }
+
+    /**
+     * Marks the task as not done.
+     */
     public void unmark() {
         isDone = false;
     }
-    public boolean isDone() { return isDone; }
-    public String getTitle() { return title; }
 
+    /**
+     * Returns whether the task is done.
+     *
+     * @return true if done, false otherwise
+     */
+    public boolean isDone() {
+        return isDone;
+    }
 
+    /**
+     * Returns the title of the task.
+     *
+     * @return the task title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Returns the string representation of the task.
+     *
+     * @return String in the format "[X] title" or "[ ] title"
+     */
     @Override
     public String toString() {
         return "[" + (isDone ? "X" : " ") + "] " + title;
